aad4fed4b4743ae1c640b729f479d5e398b92d54
h.radmehr <h.radmehr>
2023-09-13 18:23:17 +0330

    add static files url

--- daas/urls.py
+++ daas/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf.urls.static import static
+from django.views.static import serve 
 from daas import settings
 
 
@@ -24,6 +25,8 @@ urlpatterns = [
     path('@dmin/', admin.site.urls),
     path("users/",include("users.urls",namespace="users")),
     path("config/",include("config.urls",namespace="config")),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
