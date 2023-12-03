15320f21466095e1a39f2fe9882cbaf930aadea2
h.radmehr <h.radmehr>
2023-12-03 08:52:01 +0330

    add meta admin

--- users/models.py
+++ users/models.py
@@ -9,9 +9,11 @@ import uuid
 
 class Users(AbstractUser):
     email = models.CharField(max_length=100,unique=True,null=False)
+    is_meta_admin = models.BooleanField(default=False)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
 
 class Daas(models.Model):
     
